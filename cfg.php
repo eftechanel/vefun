6d0f27a96a8d823023840f63aecd460b845a545f
eftechanel <46001373+eftechanel@users.noreply.github.com>
2019-02-24 20:34:33 +0700

    Add files via upload

---
+++ cfg.php
@@ -0,0 +1,23 @@
+<?php
+
+/* Seting opsiaonal */
+$delay=30;
+// multiple account
+// akun utama nomor satu
+// jika kalian mau menambahkan akun ikuti contoh di bawah
+// if you only have one account, please delete accounts 1 and 2
+
+$config = [
+
+
+// Account :1
+"ticket=xxxxxxx&meid=xxxxxx",
+// Account :2
+"ticket=xxxxxxx&meid=xxxxxx",
+// Account :3
+"ticket=xxxxxxx&meid=xxxxxx",
+
+
+];
+
+?>
